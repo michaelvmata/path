52a99b0b1e455cd8f17f26deb1e18a604c5ffa0d
Michael Mata <michaelvmata@gmail.com>
2022-01-20 23:30:35 -0800

    Add haste buff

---
+++ buffs/haste.go
@@ -0,0 +1,31 @@
+package buffs
+
+var HasteName = "haste"
+
+type Haste struct {
+	Lifetime int
+}
+
+func (h Haste) Name() string {
+	return HasteName
+}
+
+func (h *Haste) Update(tick int) {
+	h.Lifetime--
+}
+
+func (h Haste) IsExpired() bool {
+	return h.Lifetime <= 0
+}
+
+func (h Haste) ApplyMessage() string {
+	return "The world slows perceptibly."
+}
+
+func (h Haste) UnapplyMessage() string {
+	return "The world speeds up perceptibly."
+}
+
+func NewHaste(level int) *Haste {
+	return &Haste{Lifetime: level}
+}
