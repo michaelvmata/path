e9a93bfc7449afe371c52d33f98b289fdcbc3ad3
Michael Mata <michaelvmata@gmail.com>
2023-03-19 14:07:26 -0700

    Display essence lost on death

--- actions/essenceondeath.go
+++ actions/essenceondeath.go
@@ -10,6 +10,7 @@ type EssenceOnDeath struct{}
 func (e EssenceOnDeath) Handle(World *world.World, payload events.CharacterDeathPayload) {
 	amount := payload.Character.Essence
 	payload.Character.DebitEssence(amount)
+	payload.Character.Showln("%d essence flows from you.", amount)
 	killer := payload.Killer
 	killer.Showln("%d essence flows to you.", amount)
 	killer.CreditEssence(amount)
