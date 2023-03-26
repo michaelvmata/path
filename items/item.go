501f7a9fe719fe5be1a550dda1118a1d2320b27e
Michael Mata <michaelvmata@gmail.com>
2023-03-26 10:17:38 -0700

    Use consistent styling when looking at items versus mobiles

--- items/item.go
+++ items/item.go
@@ -54,7 +54,7 @@ func (i *item) Modifiers() []modifiers.Modifier {
 
 func (i *item) Description() string {
 	parts := make([]string, 0)
-	parts = append(parts, fmt.Sprintf("Name: %s", i.Name()))
+	parts = append(parts, fmt.Sprintf("<white>Name: %s", i.Name()))
 	parts = append(parts, fmt.Sprintf("Keywords: %s", strings.Join(i.keywords, ", ")))
 	for _, modifier := range i.Modifiers() {
 		parts = append(parts, fmt.Sprintf("%s: %d", modifier.Type, modifier.Value))
